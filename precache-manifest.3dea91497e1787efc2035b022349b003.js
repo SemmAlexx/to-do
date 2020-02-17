d4c3d6db575b3fc979b46aebf37876df2ad0cf58
Bogdan Semenyk <bogdan@MacBook-Pro-Bogdan.local>
2020-02-17 21:05:40 +0200

    Updates

---
+++ precache-manifest.3dea91497e1787efc2035b022349b003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512d11fcc6514bae5f751e0732b89b11",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "277fa458b31cc8f6991b",
+    "url": "/to-do/static/css/main.cd8e6afb.chunk.css"
+  },
+  {
+    "revision": "d68a93aa308b6d61fb13",
+    "url": "/to-do/static/js/2.07fea7b0.chunk.js"
+  },
+  {
+    "revision": "ab427a65ed80b935d451c180b71c0220",
+    "url": "/to-do/static/js/2.07fea7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277fa458b31cc8f6991b",
+    "url": "/to-do/static/js/main.b5da819e.chunk.js"
+  },
+  {
+    "revision": "8b9f7010519e44a820b7",
+    "url": "/to-do/static/js/runtime-main.0ea6de14.js"
+  }
+]);
